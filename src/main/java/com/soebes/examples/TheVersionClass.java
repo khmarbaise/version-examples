2ebefea767e21202ee2cf41348408535723c17a0
Karl Heinz Marbaise <info@soebes.de>
2014-01-01 19:14:47 +0100

    - Output Implementation* and Specification*

--- src/main/java/com/soebes/examples/TheVersionClass.java
+++ src/main/java/com/soebes/examples/TheVersionClass.java
@@ -5,11 +5,22 @@ public class TheVersionClass
 
     public TheVersionClass()
     {
-        System.out.println("Implementation Version:" + this.getClass().getPackage().getImplementationVersion());
-        System.out.println(" Specification Version:" + this.getClass().getPackage().getSpecificationVersion());
+        System.out.println( "  Implementation Title:" + this.getClass().getPackage().getImplementationTitle() );
+        System.out.println( " Implementation Vendor:" + this.getClass().getPackage().getImplementationVendor() );
+        System.out.println( "Implementation Version:" + this.getClass().getPackage().getImplementationVersion() );
+        System.out.println( "    Specification Tile:" + this.getClass().getPackage().getSpecificationTitle() );
+        System.out.println( "  Specification Vendor:" + this.getClass().getPackage().getSpecificationVendor() );
+        System.out.println( " Specification Version:" + this.getClass().getPackage().getSpecificationVersion() );
+
+//        Package[] ListOfPackages = Package.getPackages();
+//        for ( Package SinglePackage : ListOfPackages )
+//        {
+//            System.out.println( "Package: " + SinglePackage.getName() );
+//        }
     }
- 
-    public static void main(String[] args) {
+
+    public static void main( String[] args )
+    {
         TheVersionClass tvc = new TheVersionClass();
     }
 }
